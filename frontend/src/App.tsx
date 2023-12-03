bc3bf524fd55bc4a98930e0bc7df1ce820999d3a
mohammed <malajaji.97@gmail.com>
2023-12-03 20:47:12 +0300

    Restaurant and menu and some styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 import Map from "./components/Map";
 import { LngLatLike } from "mapbox-gl";
@@ -11,30 +11,21 @@ import Home from "./components/Home";
 import Restaurants from "./components/Restaurants";
 import RestaurantsDetails from "./components/RestaurantsDetails";
 import Menus from "./components/Menus";
-// import dotenv from "dotenv";
 
-// dotenv.config()
-// const mapboxAccessToken = process.env["MAPBOX "]
 const mapboxAccessToken: string =
   "pk.eyJ1IjoibW9oYW1tZWRhaiIsImEiOiJjbHBmZHd5OTAxajN0MmtwZDdocTYwY3g2In0.6bA26ldyRoaxGKuFikpYRw";
-const markers: [number, number][] = [
-  [-74.5, 40.1],
-  [-74.6, 40.2],
-  [-178.4445, 47.1976],
-  // Add more marker coordinates as needed
-];
 
 function App() {
-  return (
-    <div className=" min-h-screen min-w-screen bg-gray-100 dark:bg-gray-900">
-      {/*<div className="bg-blue-500 text-white p-4">*/}
-      {/*    This is a Tailwind CSS example component.*/}
-      {/*</div>*/}
-      {/* <Restaurants /> */}
+  const location = useLocation();
+  let style = "min-h-screen min-w-screen bg-gray-100 dark:bg-gray-900";
 
-      {/* <Home/> */}
-      {/*<Navbar/>*/}
-      {/*<h1>Mapbox GL in React</h1>*/}
+  if (location.pathname === "/map") {
+    style = "bg-gray-100 dark:bg-gray-900";
+  } else {
+    style = "min-h-screen min-w-screen bg-gray-100 dark:bg-gray-900";
+  }
+  return (
+    <div className={style}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -49,17 +40,10 @@ function App() {
               accessToken={mapboxAccessToken}
               center={[46.6753, 24.7136]}
               zoom={9}
-              markers={markers}
             />
           }
         />
       </Routes>
-      {/* <Map
-        accessToken={mapboxAccessToken}
-        center={[46.6753, 24.7136]}
-        zoom={9}
-        markers={markers}
-      /> */}
     </div>
   );
 }
